c5482b707dacfb017fc11408a2135f05174afb64
Aleksey Mironenko <scrp1789@gmail.com>
2022-09-22 23:42:39 +0300

    Выполнил 41, 43 задачи

---
+++ Task41/Task41.cs
@@ -0,0 +1,43 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// Перевод строки в массив чисел
+double[] ReadArrayFromString()
+{
+    Console.Write("Введите числа: ");
+    string? inputString = Console.ReadLine();
+    if(!string.IsNullOrEmpty(inputString))
+    {
+        string[] massive = inputString.Split(' ');
+        double[] massiveNumber = new double[massive.Length];
+        for(int i = 0; i < massive.Length; i++)
+        {
+            massiveNumber[i] = double.Parse(massive[i]);
+        }
+        return massiveNumber;
+    }
+    return new double[0];
+}
+
+// Расчет количества чисел больше 0
+int CountPositiveNumber(double[] massiveNumber)
+{
+    int countPosNumber = 0;
+    for(int i = 0; i < massiveNumber.Length; i++)
+    {
+        if(massiveNumber[i] > 0)
+        {
+            countPosNumber++;
+        }
+    }
+    return countPosNumber;
+}
+
+ // Вывод количества чисел больше 0
+ void PrintResult(int countPosNumber)
+ {
+     Console.WriteLine($"Количество чисел больше 0: {countPosNumber}");
+ }
+
+double[] a = ReadArrayFromString();
+int count = CountPositiveNumber(a);
+PrintResult(count);
---
+++ Task43/Task43.cs
@@ -0,0 +1,54 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем
+
+double b1;
+double k1;
+double b2;
+double k2;
+
+// Ввод данных
+double InputData(string comment)
+{
+    Console.Write(comment);
+    double aNumber = 0;
+    string? a = Console.ReadLine();
+    if(!string.IsNullOrEmpty(a))
+    {
+        aNumber = double.Parse(a);
+    }
+    return aNumber;
+}
+
+// Нахождение точки пересечения двух прямых
+double[]? CommonPoint()
+{
+    if(k1 != k2)
+    {
+        double x;
+        double y;
+        x = (b1 - b2) / (k2 - k1);
+        y = k1 * x + b1;
+        return new double[] {x, y};
+    }
+    return null;
+}
+
+// Вывод координат точки пересечения двух прямых
+void PrintResult(double[]? point)
+{
+    if(point != null)
+    {
+        Console.WriteLine($"Координаты точки пересечения двух прямых: x = {point[0]}, y = {point[1]}");
+    }
+    else
+    {
+        Console.WriteLine("Данные прямые параллельны или совпадают");
+    }
+
+}
+
+b1 = InputData("Введите b1: ");
+k1 = InputData("Введите k1: ");
+b2 = InputData("Введите b2: ");
+k2 = InputData("Введите k2: ");
+double[]? a = CommonPoint();
+PrintResult(a);
